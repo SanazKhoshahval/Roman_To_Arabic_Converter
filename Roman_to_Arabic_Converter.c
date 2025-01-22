a67aa31e6ba43f53602827db583455a829138135
Sanaz Khoshahval <khoshahvalsanaz@gmail.com>
2025-01-22 15:07:27 -0800

    Changed the logic of roman_to_arabic function

--- Roman_to_Arabic_Converter.c
+++ Roman_to_Arabic_Converter.c
@@ -1,30 +1,38 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
-#include <string.h>
-#include <ctype.h> // For toupper function
+#include <ctype.h>
 
 int roman_to_arabic(const char roman[]);
 
-int main(void)
-{
+int main(void) {
     char roman[100];
-    printf("10 Roman numeral values will be input and converted into their equivalent Hindu-Arabic numeric values.\n");
+    int result;
+
+    printf(" 10 Roman numeral values will be input and converted into\ntheir equivalent Hindu-Arabic numeric values.\n\n");
 
     for (int i = 1; i <= 10; i++) {
         printf("Input Roman numeral # %d: ", i);
         scanf("%99s", roman);
-        printf("%s = %d\n", roman, roman_to_arabic(roman));
+
+        for (int j = 0; roman[j] != '\0'; j++) {
+            roman[j] = toupper(roman[j]);
+        }
+
+        result = roman_to_arabic(roman);
+        if (result >= 0) {  // Check if the conversion was successful
+            printf("%s = %d \n\n", roman, result);
+        }
     }
-    return 0;
+    printf("THAT'S ALL FOLKS :)\n");
 }
 
-int roman_to_arabic(const char roman[])
-{
+int roman_to_arabic(char roman[]) {
     int arabic = 0;
-    int current_value, next_value;
+    int current_value = 0;
+    int i = 0;
 
-    for (int i = 0; roman[i] != '\0'; i++) {
-        char r = toupper(roman[i]); // Handle case insensitivity
+    for (i = 0; roman[i] != '\0'; i++) {
+        char r = roman[i];
         switch (r) {
         case 'I': current_value = 1; break;
         case 'V': current_value = 5; break;
@@ -33,32 +41,30 @@ int roman_to_arabic(const char roman[])
         case 'C': current_value = 100; break;
         case 'D': current_value = 500; break;
         case 'M': current_value = 1000; break;
-        default: current_value = 0; break; // Invalid character handling
+        default:
+            // Print the part of the string up to the first invalid character
+            roman[i + 1] = '\0';  // Terminate string right after the invalid character
+            printf("%s Error - last character was not valid!!!\n\n", roman);
+            return -1;
         }
 
+        // Calculate the total value assuming the next valid characters
         if (roman[i + 1] != '\0') {
-            r = toupper(roman[i + 1]); // Next Roman numeral
-            switch (r) {
-            case 'I': next_value = 1; break;
-            case 'V': next_value = 5; break;
-            case 'X': next_value = 10; break;
-            case 'L': next_value = 50; break;
-            case 'C': next_value = 100; break;
-            case 'D': next_value = 500; break;
-            case 'M': next_value = 1000; break;
-            default: next_value = 0; break; // Invalid character handling
+            switch (roman[i + 1]) {
+            case 'I': arabic += (current_value < 1) ? -current_value : current_value; break;
+            case 'V': arabic += (current_value < 5) ? -current_value : current_value; break;
+            case 'X': arabic += (current_value < 10) ? -current_value : current_value; break;
+            case 'L': arabic += (current_value < 50) ? -current_value : current_value; break;
+            case 'C': arabic += (current_value < 100) ? -current_value : current_value; break;
+            case 'D': arabic += (current_value < 500) ? -current_value : current_value; break;
+            case 'M': arabic += (current_value < 1000) ? -current_value : current_value; break;
+            default:
+                continue;  // Keep processing if the next character is valid
             }
         }
         else {
-            next_value = 0; // No next character, so no next value
-        }
-
-        // Determine if we should add or subtract
-        if (current_value < next_value) {
-            arabic -= current_value;
-        }
-        else {
-            arabic += current_value;
+            arabic += current_value; // Add last valid character to total
+            break;  // Exit loop if no more characters
         }
     }
 
